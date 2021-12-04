5c47206cc5ec06f34520eeafe5d60ed647bbd37d
Dima Maziar <dzmaziar@mail.ru>
2021-12-04 19:26:09 +0300

    CREATED MainFrame Class

--- src/bsu/rfe/java/group8/lab45/Maziar/varB6/Main.java
+++ src/bsu/rfe/java/group8/lab45/Maziar/varB6/Main.java
@@ -1,8 +1,10 @@
 package bsu.rfe.java.group8.lab45.Maziar.varB6;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
